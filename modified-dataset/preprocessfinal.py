4480d32de1a6ea4dfc23cf06ff29784470c1aee3
kunalr2 <kunal.ranjan@kpit.com>
2019-06-20 00:45:52 +0530

    fixing preprocess final

--- modified-dataset/preprocessfinal.py
+++ modified-dataset/preprocessfinal.py
@@ -1,12 +1,23 @@
 import os
 import numpy as np
 import pandas as pd
-sensor_final = pd.read_csv("sensor_final.csv", delimiter= ",")
-oct_data = sensor_final[sensor_final["month"]=='10']
-oct_data.to_csv('oct_data.csv')
-nov_data = sensor_final[sensor_final["month"]=='11']
-nov_data.to_csv('nov_data.csv')
-dec_data = sensor_final[sensor_final["month"]=='12']
-dec_data.to_csv('dec_data.csv')
-jan_data = sensor_final[sensor_final["month"]=='01']
-jan_data.to_csv('jan_data.csv')
+sensor_final = pd.read_csv("sensor_final.csv", delimiter= ",",header =None)
+sensor_final = sensor_final.drop(sensor_final.columns[0], axis=1) 
+sensor_final = sensor_final[[4, 3, 1, 2]]
+sensor_final = sensor_final.drop(sensor_final.index[0])
+
+oct_data = sensor_final[sensor_final[4]=='10']
+oct_data = oct_data.drop(oct_data.columns[0], axis=1) 
+oct_data.to_csv('oct_data.csv', sep = '\t', mode = 'w', index=False, header = False)
+
+nov_data = sensor_final[sensor_final[4]=='11']
+nov_data = nov_data.drop(nov_data.columns[0], axis=1)
+nov_data.to_csv('nov_data.csv', sep = '\t', mode = 'w', index=False, header = False)
+
+dec_data = sensor_final[sensor_final[4]=='12']
+dec_data = dec_data.drop(dec_data.columns[0], axis=1)
+dec_data.to_csv('dec_data.csv', sep = '\t', mode = 'w', index=False, header = False)
+
+jan_data = sensor_final[sensor_final[4]=='01']
+jan_data = jan_data.drop(jan_data.columns[0], axis=1)
+jan_data.to_csv('jan_data.csv', sep = '\t', mode = 'w', index=False, header = False)
