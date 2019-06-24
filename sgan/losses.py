a9d874747946a692109874576b4b0a614799ea9e
iamrjkunal <iamrjkunal@gmail.com>
2019-06-24 09:35:32 +0000

    updating

--- sgan/losses.py
+++ sgan/losses.py
@@ -45,7 +45,7 @@ def gan_d_loss(scores_real, scores_fake):
 #     print(scores_real.size())
 #     exit()
     y_real = torch.ones_like(scores_real) * random.uniform(0.7, 1.2)
-    y_fake = torch.zeros_like(scores_fake) * random.uniform(0, 0.3)
+    y_fake = torch.ones_like(scores_fake) * random.uniform(0, 0.3)
     loss_real = bce_loss(scores_real, y_real)
     loss_fake = bce_loss(scores_fake, y_fake)
     return loss_real + loss_fake
