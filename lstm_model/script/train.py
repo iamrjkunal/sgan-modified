85ef3c80a1f53490ad7ccd6623f15f73879e8ea0
iamrjkunal <iamrjkunal@gmail.com>
2019-07-01 07:55:13 +0000

    updating train

--- lstm_model/script/train.py
+++ lstm_model/script/train.py
@@ -120,7 +120,18 @@ model.apply(init_weights)
 
 
 optimizer = optim.Adam(model.parameters())
+
 criterion = nn.MSELoss(size_average=False)
+
+# class RMSELoss(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.mse = nn.MSELoss(size_average=False)
+        
+#     def forward(self,yhat,y):
+#         return torch.sqrt(self.mse(yhat,y))
+# criterion = RMSELoss()
+
 
 
 def train(model,train_num_batches, optimizer, criterion, clip):
